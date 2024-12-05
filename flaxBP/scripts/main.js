f5db6d076b872430aed59ddeea5649120598f35c
Beardedflea <Linzell.Godfrey@live.co.uk>
2024-12-05 18:07:13 +0000

    implemented beforeOnPlace, removed playerInteract. It basically means players can treat the slab and block like any other block

--- flaxBP/scripts/main.js
+++ flaxBP/scripts/main.js
@@ -16,14 +16,14 @@ const getRelativeBlockLocation = (l, aL) => ({x:l.x + aL.x, y:l.y + aL.y, z:l.z 
 //Down,Up,North,South,West,East
 const blockFaceLocations = [{x: 0, y:1, z:0},{x: 0, y:-1, z:0},{x: 0, y:0, z:1},{x: 0, y:0, z:-1},{x: 1, y:0, z:0},{x: -1, y:0, z:0}]
 
-export function getAdjacentBlock(source, face){
+export function getAdjacentBlock(block, face){
     const faceNum = Object.keys(Direction).indexOf(face);
-    const adjacentBlock = source.dimension.getBlock(getRelativeBlockLocation(source.location, adjacentVectors[faceNum]));
+    const adjacentBlock = block.dimension.getBlock(getRelativeBlockLocation(block.location, adjacentVectors[faceNum]));
     return adjacentBlock;
 }
 
-export function getBlockFromFace(source, face){
+export function getBlockFromFace(block, face){
     const faceNum = Object.keys(Direction).indexOf(face);
-    const affectedBlock = source.dimension.getBlock(getRelativeBlockLocation(source.location, blockFaceLocations[faceNum]));
+    const affectedBlock = block.dimension.getBlock(getRelativeBlockLocation(block.location, blockFaceLocations[faceNum]));
     return affectedBlock;
 }
--- flaxBP/scripts/slabThatch.js
+++ flaxBP/scripts/slabThatch.js
@@ -3,46 +3,36 @@ import {setMainHand} from './containerUtils.js';
 import {getBlockFromFace} from './main.js'
 
 world.beforeEvents.worldInitialize.subscribe(eventData => {
-    eventData.blockComponentRegistry.registerCustomComponent('flax:on_interact_rotatable_slab', {
-        onPlayerInteract(e) {
-            const { block, player, face } = e;
-
+    eventData.blockComponentRegistry.registerCustomComponent('flax:before_on_place_rotatable_slab', {
+        beforeOnPlayerPlace(e) {
+            const {block, player, face } = e;
             const equipment = player.getComponent('equippable');
             const selectedItem = equipment.getEquipment('Mainhand');
-            if (selectedItem?.typeId === block.typeId && !block.permutation.getState('flax:double')){
 
-                const permFace = block.permutation.getState("minecraft:block_face")
-                const verticalHalf = block.permutation.getState('minecraft:vertical_half');
+            let blockToCheck = getBlockFromFace(block, face)
+
+            if (selectedItem?.typeId === blockToCheck.typeId && !blockToCheck.permutation.getState('flax:double')){
+                const permFace = blockToCheck.permutation.getState("minecraft:block_face")
+                const verticalHalf = blockToCheck.permutation.getState('minecraft:vertical_half');
 
                 const axisNum = Math.floor(Object.keys(Direction).indexOf(permFace.charAt(0).toUpperCase() + permFace.slice(1)) / 2)
 
                 const isBottomUp = verticalHalf === 'bottom' && face === 'Up';
                 const isTopDown = verticalHalf === 'top' && face === 'Down';
-
+                
                 if (isBottomUp || isTopDown) {
 
-                    block.setPermutation(block.permutation.withState('flax:double', true)); 
-                    block.setPermutation(block.permutation.withState('flax:axis', axisNum));
+                    blockToCheck.setPermutation(blockToCheck.permutation.withState('flax:double', true)); 
+                    blockToCheck.setPermutation(blockToCheck.permutation.withState('flax:axis', axisNum));
+                    world.playSound('use.grass', blockToCheck.location);
+                    e.cancel = true;
                     setMainHand(player, equipment, selectedItem);
-                    world.playSound('use.grass', block.location);
-
                 }
                 
             }
-        }
+        },
     });
 });
-// world.beforeEvents.worldInitialize.subscribe(eventData => {
-//     eventData.blockComponentRegistry.registerCustomComponent('flax:before_on_place_rotatable_slab', {
-//         beforeOnPlayerPlace(e) {
-//             const {permutationToPlace, face} = e;
-//             const axisNum = Math.floor(Object.keys(Direction).indexOf(face.charAt(0).toUpperCase() + face.slice(1)) / 2)
-//             const isRotatable = axisNum === 0 ? true : false;
-
-//             e.permutationToPlace = permutationToPlace.withState('flax:rotatable_slab', isRotatable);
-//             }
-//     });
-// });
 
 world.beforeEvents.worldInitialize.subscribe(eventData => {
     eventData.blockComponentRegistry.registerCustomComponent('flax:on_player_destroy_slab', {
