877f443137ee0219362dcd0eb6796a6da4488464
Beardedflea <Linzell.Godfrey@live.co.uk>
2026-09-25 00:42:40 +0100

    flax crop updated to 1.26.40. Updated script component

--- flaxBP/scripts/cropFlax.js
+++ flaxBP/scripts/cropFlax.js
@@ -1,90 +1,138 @@
 import {system, ItemStack} from '@minecraft/server';
 import {setMainHand} from './containerUtils.js';
 
-system.beforeEvents.startup.subscribe(eventData => {
-    eventData.blockComponentRegistry.registerCustomComponent('flax:grow_basic', {
-        onRandomTick(e) {
-            const { block } = e;
-            const cropStage = block.permutation.getState('flax:growth_stage');
-            if(Math.floor(Math.random() * 100) < 20 && cropStage != 3){
-                block.setPermutation(block.permutation.withState('flax:growth_stage', cropStage+1));
-            }
-        }
-    });
+
+system.beforeEvents.startup.subscribe(({ blockComponentRegistry }) => {
+    blockComponentRegistry.registerCustomComponent(
+        'flax:crop_growth',
+        FlaxCropGrowthComponent
+    );
 });
-/*Vanilla crops have a ~14% probability per tick to enter the next growth stage. This probability is increased by the number of surrounding
-    *farmland blocks and whether they are hydrated. The max probability is ~33% with 8 farmland blocks surrounding a crop.
-    *Taking this all into consideration, most vanilla crops take an average of 31 minutes to fully grow. 
-    *Source: https://minecraft.fandom.com/wiki/Tutorials/Crop_farming#Growth_rate
-    *With that being said, I've reduced the chance from 100% down to 24%. Mean average shit. I changed my mind, im moving it down to 20%
-*/ 
-system.beforeEvents.startup.subscribe(eventData => {
-    eventData.blockComponentRegistry.registerCustomComponent('flax:fertilize', {
-        onPlayerInteract(e) {
-            const { block, player, dimension} = e;
+
+const FlaxCropGrowthComponent = {
+    onRandomTick({ block }, { params }) {
+        // Growth parameters
+        const growthState = params.growth_state;
+        const maxGrowth = params.max_growth;
+        const minLightLevel = params.min_light_level;
+
+        if (block.getLightLevel() < minLightLevel) return;
+
+        const { permutation } = block;
+
+        const growth = permutation.getState(growthState) ?? maxGrowth;
+
+        if (growth === maxGrowth) return;
+
+        if (!randomShouldCropGrow(block, params)) return;
+
+        block.setPermutation(permutation.withState(growthState, growth + 1));
+    },
+    onPlayerInteract({ block, dimension, player }, { params }) {
 
             if(!player || !player.getComponent('equippable')) return;
             
             const equipment = player.getComponent('equippable');
             const selectedItem = equipment.getEquipment('Mainhand');
-            const cropStage = block.permutation.getState('flax:growth_stage');
-            const maxGrowth = block.permutation.getState('flax:max_stage');
 
             if (!selectedItem || selectedItem.typeId != "minecraft:bone_meal") return;
             
+            const growthState = params.growth_state;
+            const growthRange = params.growth_on_fertilize;
+            const maxGrowth = params.max_growth;
+            
             if (player.getGameMode() === "Creative"){
-                block.setPermutation(block.permutation.withState('flax:growth_stage', maxGrowth)); 
+                block.setPermutation(block.permutation.withState(growthState, maxGrowth)); 
             }
             else{
-                //Since we are always rounding down, we can set the max value +1 above the max growth stage
-                const newStage = Math.floor(Math.random() * ((maxGrowth+1) - cropStage) + cropStage)
-                block.setPermutation(block.permutation.withState('flax:growth_stage', newStage));
+                let growth = block.permutation.getState(growthState);
+
+                growth += randomInt(...growthRange);
+                growth = Math.min(growth, maxGrowth);
+
+                block.setPermutation(block.permutation.withState(growthState, growth));
             }
+
             setMainHand(player, equipment, selectedItem);
 
-            dimension.playSound('item.bone_meal.use', block.location);
+            dimension.playSound('item.bone_meal.use', block.center());
+            dimension.spawnParticle("minecraft:crop_growth_emitter", block.center())
+    }
+}
 
-            block.dimension.spawnParticle("minecraft:crop_growth_emitter", block.center())
+function getGrowthSpeed(crop, growthParams) {
+    let speed = 1;
+
+    // Increase growth speed based on nearby farmland blocks and their moisture
+    for (const farmland of getFarmlandIterator(crop, growthParams.farmland_search_range)) {
+        let speedModifier = growthParams.farmland_speed_modifier;
+
+        const moisture = farmland.permutation.getState("moisturized_amount");
+        if (moisture > 0) {
+            speedModifier += growthParams.farmland_moisture_speed_modifier;
         }
-    });
-});
-//Don't need as "minecraft:liquid_detection is now a thing". Keeping just in case i need something that utilises something similar(im a hoarder)
-// world.beforeEvents.worldInitialize.subscribe(eventData => {
-//     eventData.blockComponentRegistry.registerCustomComponent('flax:destroy_crop_water', {
-//         onTick(e) {
-//             try{
-//                 const { block } = e;
-//                 const surroundingBlocks = [block.above(), block.north(), block.south(), block.west(), block.east()]
-//                 const findWater = surroundingBlocks.find(block => block.typeId === "minecraft:water");
 
-//                 if(findWater === undefined) return;
-//                 //console.warn(`data: §7${JSON.stringify(findWater.permutation.getAllStates(), null, 4)}`);
-//                 const cropStage = block.permutation.getState('flax:growth_stage');
-//                 const maxGrowth = block.permutation.getState('flax:max_stage');
+        const isDirectlyBelowCrop = farmland.x === crop.x && farmland.z === crop.z;
+        if (!isDirectlyBelowCrop) {
+            speedModifier *= growthParams.neighboring_farmland_speed_multiplier;
+        }
 
+        speed += speedModifier;
+    }
 
-//                 if(cropStage === maxGrowth){
-//                     const flax = Math.floor(Math.random() * 2) + 1;
-//                     const seed = Math.floor(Math.random() * 2);
-//                     block.dimension.spawnItem(new ItemStack("flax:flax_fibre", flax), block.location)
+    // Halves the growth speed if there are surrounding crops of the same type in vanilla (where "crowding_speed_multiplier" is 0.5)
+    if (isCrowded(crop)) {
+        speed *= growthParams.crowding_speed_multiplier;
+    }
 
-//                     if(seed != 0){
-//                         block.dimension.spawnItem(new ItemStack("flax:flax_seeds", seed), block.location)
-//                     }
-//                 }
-//                 else{
-//                     block.dimension.spawnItem(new ItemStack("flax:flax_seeds", 1), block.location)
-//                 }
-//                 block.setType("minecraft:air");
-//                 world.playSound('dig.grass', block.location);
-//             } catch (error) {
-//                 console.error()
-//             }
-//         }
-//     });
-// });
-//TODO: Current iteration cannot determine the flow direction of water, hence the delicate nature of
-//crops being destroyed from the sheer staring power of flowing liquids.
+    return speed;
+}
+
+function* getFarmlandIterator(crop, searchRange) {
+    for (let x = -searchRange; x <= searchRange; x++) {
+        for (let z = -searchRange; z <= searchRange; z++) {
+            const block = crop.offset({ x, y: -1, z });
+
+            // Yield the block if it is farmland
+            const isFarmland = block?.typeId === "minecraft:farmland";
+            if (isFarmland) yield block;
+        }
+    }
+}
+
+function isCrowded(crop) {
+    const northBlock = crop.north();
+    const southBlock = crop.south();
+    const westBlock = crop.west();
+    const eastBlock = crop.east();
+
+    const isEnclosed =
+        (westBlock?.typeId === crop.typeId || eastBlock?.typeId === crop.typeId) &&
+        (northBlock?.typeId === crop.typeId || southBlock?.typeId === crop.typeId);
+
+    if (isEnclosed) return true;
+
+    const isCropDiagonallyAdjacent =
+        northBlock?.west()?.typeId === crop.typeId ||
+        northBlock?.east()?.typeId === crop.typeId ||
+        southBlock?.west()?.typeId === crop.typeId ||
+        southBlock?.east()?.typeId === crop.typeId;
+
+    if (isCropDiagonallyAdjacent) return true;
+
+    return false;
+}
+
+function randomInt(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+}
+
+export function randomShouldCropGrow(crop, growthParams) {
+    const growthSpeed = getGrowthSpeed(crop, growthParams);
+    const growthChanceRange = Math.floor(25 / growthSpeed);
+
+    return randomInt(0, growthChanceRange) === 0;
+}
 
 system.beforeEvents.startup.subscribe(eventData => {
     eventData.blockComponentRegistry.registerCustomComponent('flax:on_player_destroy_flax', {
